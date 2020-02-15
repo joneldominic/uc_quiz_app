95e7803ee8d9257a1f644a08534db54580b638df
joneldominic <dneljo@yahoo.com>
2020-02-15 23:26:20 +0800

    35. Updating correctly with stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(QuizApp());
 
-class QuizApp extends StatelessWidget {
-  void answerQuestion() {
-    print("Answer choosen!");
+class QuizApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _QuizAppState();
   }
+}
 
- void answerQuestionWithParam(int num) {
-    print("Answer choosen, $num");
+class _QuizAppState extends State<QuizApp> {
+  var questions = [
+    "What's your favorite color?",
+    "What's your favorite animal?"
+    "What's your name?"
+  ];
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() { // Inform the class that something has changed...
+      questionIndex < 2 ? questionIndex++ : questionIndex = 0;
+    });
+    print("Answer choosen!, question index = $questionIndex");
+    print(questions[questionIndex]);
   }
 
   @override
   Widget build(BuildContext context) {
-    var question = [
-      "What's your favorite color?",
-      "What's your favorite animal?"
-    ];
-
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
@@ -25,20 +34,18 @@ class QuizApp extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text("Hello World"),
+          Text(questions[questionIndex]),
           RaisedButton(
             child: Text("Answer 1"),
             onPressed: answerQuestion,
           ),
           RaisedButton(
             child: Text("Answer 2"),
-            onPressed: () => answerQuestionWithParam(2),
+            onPressed: answerQuestion,
           ),
           RaisedButton(
             child: Text("Answer 3"),
-            onPressed: (){
-              print("Answer choosen, 3");
-            },
+            onPressed: answerQuestion,
           ),
         ],
       ),
